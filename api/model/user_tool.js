88e4436fef458d0aff4126bde12ee8d08e1193f7
devdetj <daviddetj@gmail.com>
2017-10-22 01:17:20 +0200

    base

--- api/model/user_tool.js
+++ api/model/user_tool.js
@@ -2,34 +2,37 @@ var db = require('./extends/db.js');
 
 module.exports = function register(name,passwd,email,callback){
     var dbs = new db();
-    dbs.insert('user',JSON.stringify({ name:name, passwd:passwd, email:email }), function(success){
+    dbs.insert('user',{ name:name, passwd:passwd, email:email }, function(success){
 
-            callback(success);
-            dbs = null;
+        callback(success);
+        dbs = null;
 
     });
     dbs = null;
 
 };
-module.exports = function remove(user, callback){
-  var dbs = new db();
-  dbs.remove('user',JSON.stringify(user), function(success){
 
-      callback(success);
-      dbs = null;
+module.exports = function remove(name,passwd,email, callback){
+    var dbs = new db();
+    dbs.remove('user',{name:name,passwd:passwd,email:email}, function(success){
 
-  });
-  dbs = null;
+        callback(success);
+        dbs = null;
+
+    });
+    dbs = null;
 
 };
 module.exports = function login(name,passwd,callback){
 
-    dbs.find('user',JSON.stringify({name:name,passwd:passwd}),function(user){
+    dbs.find('user',{name:name,passwd:passwd},function(user){
 
         callback(user);
         dbs = null;
     });
     dbs = null;
 };
+
+
 
 
