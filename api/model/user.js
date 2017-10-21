8d41a27a7e49958a14604ae1be837dc366d79776
devdetj <daviddetj@gmail.com>
2017-10-22 00:14:53 +0200

    base

---
+++ api/model/user.js
@@ -0,0 +1,26 @@
+var name = require('./extends/name.js');
+console.log(name);
+
+class pre_user extends name{
+
+    constructor(name,passwd,email){
+
+        super(name);
+        this.passwd=passwd;
+        this.email=email;
+
+    }
+
+}
+
+class user extends pre_user{
+
+    constructor(name,passwd,email){
+
+        super(name,passwd,email);
+
+    }
+
+}
+
+module.exports = user;
