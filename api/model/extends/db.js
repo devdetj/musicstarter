f0d39534042f1a17071ece7cd499b2ef8b7b4217
devdetj <daviddetj@gmail.com>
2017-10-22 00:49:29 +0200

    base

---
+++ api/model/extends/db.js
@@ -0,0 +1,74 @@
+var MongoClient = require('mongodb').MongoClient;
+
+class db{
+    insert(clt,data,callback){
+
+        MongoClient.connect("mongodb://localhost:27017/musicstarter", function(err, db) {
+
+            if (err) {
+
+                console.error(err);
+            }
+
+            db.collection(clt).insert(data,function(err2, docs) {
+
+                if (err2) {
+
+                    console.error(err2);
+
+                }
+                if(docs.ops[0] !== undefined) {
+
+                    callback(docs.ops[0]);
+
+                }else {
+
+                    callback(false);
+                }
+            });
+
+        });
+    }
+    remove(clt,data,callback){
+        MongoClient.connect("mongodb://localhost:27017/musicstarter", function(err, db) {
+
+            if (err) {
+
+                console.error(err);
+            }
+
+            db.collection(clt).remove(data,function(err2, docs) {
+
+                if (err2) {
+
+                    console.error(err2);
+
+                }
+                callback(docs);
+            });
+        });
+    }
+    find(clt,data,callback){
+        MongoClient.connect("mongodb://localhost:27017/musicstarter", function(err, db) {
+
+            if (err) {
+
+                console.error(err);
+            }
+
+            db.collection(clt).find(data,function(err2, docs) {
+
+                if (err2) {
+
+                    console.error(err2);
+
+                }
+                callback(docs);
+            });
+        });
+
+    }
+
+}
+
+module.exports = db;
