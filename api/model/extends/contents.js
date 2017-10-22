357b3489eb1468e5f8c32a5ddfd0c2de5be9fef1
devdetj <daviddetj@gmail.com>
2017-10-22 03:04:26 +0200

    base

---
+++ api/model/extends/contents.js
@@ -0,0 +1,10 @@
+var id_dad = require('./id_dad.js');
+class contents extends id_dad{
+
+    constructor(id_dad,name){
+
+        super(id_dad,name);
+
+    }
+
+}
---
+++ api/model/extends/id_dad.js
@@ -0,0 +1,11 @@
+var name = require('./name.js');
+class id_dad extends name{
+    constructor(id_dad,name){
+
+        this.id_dad=id_dad;
+        super(name);
+
+    }
+
+}
+module.exports=id_dad;
