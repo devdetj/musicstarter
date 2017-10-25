f716ddf0f178c36bfa1d78d59fc26092d291d99f
JoanShadow <joanpecamp1993@gmail.com>
2017-10-25 18:19:01 +0200

    new front_auth

--- api/controller/register.js
+++ api/controller/register.js
@@ -1,60 +1,43 @@
-/*document.getElementByName('email').addEventListener('input', function() {
-    campo = event.target;
-    //valido = document.getElementById('emailOK');
-
-    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
-    if (emailRegex.test(campo.value)) {
-      console.log("valido");
-      //valido.innerText = "válido";
-    } else {
-    	console.log("Formato email incorrecto");
-      //valido.innerText = "incorrecto";
-    }
-});*/
+document.getElementById("submit").onclick = function(evnt) {
+    var valido = validate();
+    if (!valido) return false;
+}
 
 function validate() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var passwd = document.getElementById("passwd");
 
-	var name = document.getElementById("name").value;
-	var email = document.getElementById("email").value;
-	var passwd = document.getElementById("passwd").value;
-	var contador = 0;
-	console.log(name + "" + email + "" + passwd);
+    var mssg = "";
 
-	// Validación nombre
-	if (name == 0 || name == null) { //|| /^\s+$/.test(name)
-        document.getElementById("warning_name").innerHTML =
-            "El campo nombre no puede ir vacío, ser nulo, ni ser de tipo numérico";
-        contador--;
-    } else {
-        document.getElementById("success_name").innerHTML = "Succes!";
-        contador++;
-	}
+    // Name validator
+    if(!name.validity.valid) {
+        if(name.validity.tooShort) mssg +=
+            "<p>El nombre és demasiado corto</p>";
+        if(name.validity.tooLong) mssg +=
+            "<p>El nombre no puede contener más de 32 carácteres</p>";
+        if(name.validity.patternMismatch) mssg +=
+            "<p>El nombre solo puede contener letras</p>";
+        if(name.validity.valueMissing) mssg +=
+            "<p>El campo nombre, no puede ir vacío</p>";
+    }
 
-	// Validación email
-	if (email == 0 || email == null) {
-	    document.getElementById("warning_email").innerHTML =
-            "El campo email no puede ir vacío, ser nulo, ni de tipo numérico";
-		contador--;
-	} else {
-	    document.getElementById("success_email").innerHTML = "Success!";
-		contador++;
-	}
+    // Email validator
+    if(!email.validity.valid) {
+        if(email.validity.valueMissing) mssg +=
+            "<p>El campo email, no puede ir vacío</p>";
+    }
 
-	// Validación contraseña
-	if (passwd == 0 || passwd == null) {
-	    document.getElementById("warning_passwd").innerHTML = "El campo contraseña no puede ir vacío, ni ser nulo";
-	    contador--;
-	} else {
-	    document.getElementById("success_passwd").innerHTML = "Success!";
-		contador++;
-	}
-
-	// Validación general
-	if (contador == 3) {
-		document.getElementById("success_general").innerHTML = "Success all!";
-		document.getElementById("submit").classList.add("btn_submit_success");
-	} else {
-	    document.getElementById("warning_general").innerHTML = "Invalid form!";
-	}
+    // Password validator
+    if(!passwd.validity.valid) {
+        if(passwd.validity.tooShort) mssg +=
+            "<p>La contraseña debe ser de almenos 6 carácteres</p>";
+        if(passwd.validity.valueMissing) mssg +=
+            "<p>El campo contraseña, no puede ir vacío</p>"
+    }
+    document.getElementById("errors").innerHTML = "<strong>" + mssg + "</strong>";
+    document.getElementById("errors").style = "display: block";
+    return mssg?false:true;
 }
+
+//document.getElementById("submit").classList.add("btn_submit_success");
